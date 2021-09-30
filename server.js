dd1a27a7f2194a69e5b999c31037f5559a096e0b
vigor-lundkvist <vigor.lundkvist@live.se>
2021-09-30 18:14:56 +0200

    Another commit.

--- server.js
+++ server.js
@@ -19,13 +19,21 @@ app.get('/', (req, res) => {
 
 
 app.get('/stylesheet.css', (req, res) => {
-    fs.readFile('stylesheet.css', (err, data) => {
+    fs.readFile('stylessheet.css', (err, data) => {
         res.setHeader('Content-Type', 'text/css');
         res.send(data);
         
     });
+    
 })
- 
+
+app.get('/worksofc.jpg', (req, res) => {
+    fs.readFile('worksofc.jpg', (err, data) => {
+        res.setHeader('Content-Type', 'image/jpg');
+        res.send(data);
+        
+    });
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
