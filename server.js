62798d11fd0748bb59944e341a8c44ec58e0c612
vigor-lundkvist <vigor.lundkvist@live.se>
2021-09-30 19:30:54 +0200

    another commit.

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 const PORT = process.env.PORT || 8042;
-const fs = require('fs');
- 
 
 app.get('/', (req, res) => {
-    fs.readFile('app.html', (err, data) => {
-        
-        res.setHeader('Content-Type', 'text/html');
-        res.setHeader('Content-Length', data.length);
-        res.send(data);
-        
-    });
-  
-  })
 
+    fs.readFile('app.html', (err, data) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(data);
+    });
+
+});
 
 app.get('/stylesheet.css', (req, res) => {
-    fs.readFile('stylessheet.css', (err, data) => {
-        res.setHeader('Content-Type', 'text/css');
+
+    fs.readFile('stylesheet.css', (err, data) => {
         res.send(data);
-        
     });
-    
-})
+
+});
 
 app.get('/worksofc.jpg', (req, res) => {
+
     fs.readFile('worksofc.jpg', (err, data) => {
-        res.setHeader('Content-Type', 'image/jpg');
         res.send(data);
-        
     });
-})
+
+});
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-  })
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
