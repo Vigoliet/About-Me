1a8cecd7ef6488325b9cc3b2e62cd94da2164f78
vigor-lundkvist <vigor.lundkvist@live.se>
2021-10-04 11:10:39 +0200

    Better version of server, no need for app.get.

--- server.js
+++ server.js
@@ -6,26 +6,12 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 8042;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
 
-    fs.readFile('app.html', (err, data) => {
+    fs.readFile('public/app.html', (err, data) => {
         res.setHeader('Content-Type', 'text/html');
-        res.send(data);
-    });
-
-});
-
-app.get('/stylesheet.css', (req, res) => {
-
-    fs.readFile('stylesheet.css', (err, data) => {
-        res.send(data);
-    });
-
-});
-
-app.get('/worksofc.jpg', (req, res) => {
-
-    fs.readFile('worksofc.jpg', (err, data) => {
         res.send(data);
     });
 
